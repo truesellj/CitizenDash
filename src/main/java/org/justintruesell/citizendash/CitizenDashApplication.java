cdbadfaaa2f895d6f8196979fb92c26ed8564878
truesellj <truesellj@gmail.com>
2024-03-14 10:10:24 -0700

    first committ

---
+++ src/main/java/org/justintruesell/citizendash/CitizenDashApplication.java
@@ -0,0 +1,13 @@
+package org.justintruesell.citizendash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/justintruesell/citizendash/CitizenDashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.justintruesell.citizendash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
