323b4a6a5557a827c7e8fb7eceed7f64e4776473
truesellj <truesellj@gmail.com>
2024-03-14 13:37:56 -0700

    forgot to put lombok annotations on Todo, this corrects that

--- src/main/java/org/justintruesell/citizendash/models/Todo.java
+++ src/main/java/org/justintruesell/citizendash/models/Todo.java
@@ -3,7 +3,12 @@ package org.justintruesell.citizendash.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Todo {
     @Id
