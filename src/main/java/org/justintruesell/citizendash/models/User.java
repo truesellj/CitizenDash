f5051ba18a5aa1cada498dfefb46910e524d3a53
truesellj <truesellj@gmail.com>
2024-03-14 11:30:25 -0700

    Setup application.properties file

--- src/main/java/org/justintruesell/citizendash/models/User.java
+++ src/main/java/org/justintruesell/citizendash/models/User.java
@@ -1,4 +1,17 @@
 package org.justintruesell.citizendash.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String address;
+    private int dob;
 }
