ee6372102d7be1eaa7f179eb825b2a8e2b8558e3
truesellj <truesellj@gmail.com>
2024-03-14 10:15:54 -0700

    attempt to commit all file

--- src/main/java/org/justintruesell/citizendash/CitizenDashApplication.java
+++ src/main/java/org/justintruesell/citizendash/CitizenDashApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CitizenDashApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CitizenDashApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CitizenDashApplication.class, args);
+    }
 
 }
